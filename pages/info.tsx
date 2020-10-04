5544d7fd5b41d6ea8dd21f4126fa84473be3a6d9
wonder <gonzalezwonderly@gmail.com>
2020-10-04 01:08:08 -0400

    Initial Info page

--- pages/info.tsx
+++ pages/info.tsx
@@ -2,40 +2,49 @@ import React from "react";
 import { Flex, Box } from "@chakra-ui/core";
 import { Header } from "../components/atoms/Text";
 import Standard from "../components/layouts/Standard/Standard";
-import { Split } from "../components/layouts/Split";
 
 const Info = () => {
   return (
     <Standard>
-      <Flex justify="center" width="100%" align="center" height="75%">
+      <Flex
+        justify="center"
+        width="100%"
+        align="center"
+        height="75%"
+        backgroundImage="url('/images/cupcakes-inline.jpg')"
+        backgroundPosition="center"
+        backgroundSize="cover"
+      >
         <Header>Catalogo</Header>
       </Flex>
-      <Split>
-        <Flex justify="center" align="center" width="100%" height="100%">
+      <Flex>
+        <Flex justify="center" align="center" width={{ sm: "100%", md: "100%", lg: "50%", xl: "50%" }} height="100vh">
           <Header>Mision</Header>
         </Flex>
         <Box
           bg="rgb(255,0,0)"
-          w="100%"
-          h="100%"
+          w="50%"
+          display={{ sm: "none", md: "none", lg: "block", xl: "block" }}
+          h="100vh"
           backgroundImage="url('/images/mision.jpg')"
           backgroundPosition="center"
           backgroundSize="cover"
         />
-      </Split>
-      <Split>
+      </Flex>
+      <Flex>
         <Box
           bg="rgb(255,0,0)"
-          w="100%"
-          h="100%"
+          w="50%"
+          display={{ sm: "none", md: "none", lg: "block", xl: "block" }}
+          h="100vh"
           backgroundImage="url('/images/vision.jpg')"
           backgroundPosition="center"
           backgroundSize="cover"
         />
-        <Flex justify="center" align="center" width="100%" height="100%">
+        <Flex justify="center" align="center" width={{ sm: "100%", md: "100%", lg: "50%", xl: "50%" }} height="100vh">
           <Header>Vision</Header>
         </Flex>
-      </Split>
+      </Flex>
     </Standard>
   );
 };
