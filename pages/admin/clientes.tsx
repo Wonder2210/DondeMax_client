d95cce02534718451fb9285c5eaf9900f161ce49
Wonder2210 <gonzalezwonderly@gmail.com>
2020-12-06 17:00:42 -0400

    Update clientes.tsx

--- pages/admin/clientes.tsx
+++ pages/admin/clientes.tsx
@@ -89,7 +89,7 @@ function clientes() {
       <Head>
         <title>Admin - Clientes</title>
       </Head>
-      { loafing ? (
+      { loading ? (
         <Animation />
       ) : (
         <>
