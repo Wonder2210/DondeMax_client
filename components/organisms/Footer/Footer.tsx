8f008a0a232ab74afa80d4f75bdf384efa97be23
Wonder <gonzalezwonderly@gmail.com>
2020-12-08 00:09:34 -0400

    reorganized footer

--- components/organisms/Footer/Footer.tsx
+++ components/organisms/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-import { Flex } from "@chakra-ui/core";
+import { Flex, Heading } from "@chakra-ui/core";
 import { Icon } from "@iconify/react";
 import Instagram from "@iconify/icons-cib/instagram";
 import Whatsapp from "@iconify/icons-cib/whatsapp";
@@ -14,51 +14,100 @@ const Footer = () => {
     maxDeviceWidth: 768,
   });
   return (
-    <Flex
-      w="100%"
-      direction={isMobile ? "column" : "row"}
-      backgroundColor="#3B3E4A"
-      justify="space-evenly"
-      padding="3em"
-      position="relative"
-      bottom="0"
-    >
-      <Flex justify="center" align="center">
-        <Header fontSize="3.5em" color="#FFF">
-          DondeMax
-        </Header>
-      </Flex>
-      <List width="auto" spacing={3}>
-        <ListItem marginLeft={3}>
-          <Link href="/about">
-            <a>
-              <StyleLink _hover={{ color: "rose.600", transform: "scale(1.1)" }} color="white" fontSize="xl2">
-                Acerca de Nosotros
-              </StyleLink>
-            </a>
-          </Link>
-        </ListItem>
-       
-        <ListItem marginLeft={3}>
-          <Link href="/" passHref>
-            <a>
-              <StyleLink _hover={{ color: "rose.600", transform: "scale(1.1)" }} color="white" fontSize="xl2">
-                Pedidos
-              </StyleLink>
-            </a>
-          </Link>
-        </ListItem>
-      </List>
-      <Flex direction="row" w="8em" justifyContent="space-between">
-        <StyleLink href="/">
-          <Icon icon={Instagram} color="#FFF" width="3em" height="3em" />
-        </StyleLink>
+    <>
+      <style jsx>{`
+        a {
+          color: white;
 
-        <StyleLink href="/">
-          <Icon icon={Whatsapp} color="#FFF" width="3em" height="3em" />
-        </StyleLink>
+          transition: color 275ms ease;
+        }
+        a:hover {
+          color: #e91e63;
+        }
+      `}</style>
+      <Flex
+        w="100%"
+        direction={isMobile ? "column" : "row"}
+        backgroundColor="#3B3E4A"
+        justify="space-evenly"
+        padding="3em"
+        position="relative"
+        bottom="0"
+      >
+        <Flex justify="center" flexDirection="column" align="center">
+          <Header fontSize="3.5em" color="colors.rose.600">
+            D
+          </Header>
+          <Header fontSize="3.5em" color="#FFF">
+            onde
+          </Header>
+          <Header fontSize="3.5em" color="colors.rose.600">
+            M
+          </Header>
+          <Header fontSize="3.5em" color="#FFF">
+            ax
+          </Header>
+        </Flex>
+        <List width="auto" spacing={3}>
+          <Heading color="white" fontSize="xl">
+            Sitio:
+          </Heading>
+          <ListItem marginLeft={3}>
+            <Link href="/about">
+              <a>Acerca de Nosotros</a>
+            </Link>
+          </ListItem>
+          <ListItem marginLeft={3}>
+            <Link href="/about">
+              <a>Pedidos</a>
+            </Link>
+          </ListItem>
+
+          <ListItem marginLeft={3} color="white">
+            <Link href="/store">
+              <a>Tienda</a>
+            </Link>
+          </ListItem>
+        </List>
+        <Flex flexDirection="column" w="9em">
+          <Heading color="white" fontSize="xl">
+            Contactanos:
+          </Heading>
+          <List width="auto" spacing={3}>
+            <ListItem>
+              <a href="">
+                <Icon
+                  style={{
+                    display: "inline",
+                    margin: "0.2em",
+                  }}
+                  icon={Instagram}
+                  color="#FFF"
+                  width="1.5em"
+                  height="1.5em"
+                />
+                Instagram
+              </a>
+            </ListItem>
+            <ListItem>
+              <a href="">
+                <Icon
+                  style={{
+                    display: "inline",
+                    margin: "0.2em",
+                  }}
+                  icon={Whatsapp}
+                  color="#FFF"
+                  width="1.5em"
+                  height="1.5em"
+                />
+                Whatsapp
+              </a>
+            </ListItem>
+          </List>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
